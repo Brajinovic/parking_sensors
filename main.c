d6c852d6534d12f0157ed95fdaafc9a767d18b59
Brajinovic <ivan.brajinovic@outlook.com>
2024-11-24 11:25:03 +0100

    changed the coordinate system

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include <errno.h>
 
+#define WINDOW_WIDTH 720
+#define WINDOW_HEIGHT 720
+#define DEBUG 0
+
 unsigned char* loadPPM(const char* filename, int* width, int* height) {
 	const int BUFSIZE = 128;
 	FILE* fp;
@@ -43,9 +47,37 @@ unsigned char* loadPPM(const char* filename, int* width, int* height) {
 
 		return NULL;
 	}
+
+#if DEBUG == 1
+	printf("\nloadPPM rawData[0]: %d\n", rawData[0]);
+#endif
+
 	return rawData;
 }
 
+
+void loadTexture()
+{
+	GLuint texture[1]; // declaring space for one texture
+	int twidth, theight; // declaring variable for width and height of an image
+	unsigned char* tdata; // declaring pixel data
+						  // loading image data from specific file:
+	tdata = loadPPM("auto3.ppm", &twidth, &theight);
+	if (tdata == NULL) return; // check if image data is loaded
+							   // generating a texture to show the image
+
+
+#if DEBUG == 1
+	printf("\nloadTexture tdata[0]: %d\n", tdata[0]);
+#endif
+
+
+	glGenTextures(1, &texture[0]);
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, twidth, theight, 0, GL_RGB, GL_UNSIGNED_BYTE, tdata);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+}
 
 void initGL()
 {
@@ -54,7 +86,7 @@ void initGL()
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // get clear background (black color)
 	glClearDepth(1.0f); // color depth buffer
 	glDepthFunc(GL_LEQUAL); // configuration of depth testing
-							//enable additional options regarding: perspective correction, anti-aliasing, etc
+							// enable additional options regarding: perspective correction, anti-aliasing, etc
 	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 	glEnable(GL_LINE_SMOOTH);
 	glEnable(GL_POLYGON_SMOOTH);
@@ -73,9 +105,22 @@ void reshape(int width, int height)
 	// clean projection matrix
 	glLoadIdentity();
 	// set camera view (orthographic projection with 4x4 unit square canvas)
-	glOrtho(-2, 2, -2, 2, 2, -2);
+	glOrtho(0, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 720, 0);
 	// swith back to matrix
 	glMatrixMode(GL_MODELVIEW);
+}
+
+void draw_canvas(int width, int height)
+{
+
+	// create the canvas over the whole window
+	// pick the color (white in this case)
+	glColor3f(1.0, 1.0, 1.0);
+	// draw the canvas over the given area
+	glTexCoord2f(0, WINDOW_HEIGHT); glVertex3f(0, 0, 0);
+	glTexCoord2f(WINDOW_WIDTH, WINDOW_HEIGHT); glVertex3f(width, 0, 0);
+	glTexCoord2f(WINDOW_WIDTH, 0); glVertex3f(width, height, 0);
+	glTexCoord2f(0, 0); glVertex3f(0, height, 0);
 }
 
 
@@ -85,31 +130,27 @@ void display() {
 	
 
 	glBegin(GL_QUADS);
-	glColor3f(1.0, 1.0, 1.0);
-	// coordinates of initial white rectangle for the background
-	glTexCoord2f(0, 1); glVertex3f(-2, -1, 0);
-	glTexCoord2f(1, 1); glVertex3f(2, -1, 0);
-	glTexCoord2f(1, 0); glVertex3f(2, 1, 0);
-	glTexCoord2f(0, 0); glVertex3f(-2, 1, 0);
 
+	draw_canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+	
 
 	glColor3f(1.0, 0.0, 0.0);   //choosing red color
-	glVertex3f(-1.20f, -0.20f, 0.0f);   
-	glVertex3f(-1.00f, -0.20f, 0.0f);
-	glVertex3f(-1.00f, -0.00f, 0.0f);
-	glVertex3f(-1.20f, -0.00f, 0.0f);
-
+	glVertex3f(0.0f, 0.0f, 0.0f);   
+	glVertex3f(50.00f, 0.0f, 0.0f);
+	glVertex3f(50.00f, 50.00f, 0.0f);
+	glVertex3f(0.0f, 50.00f, 0.0f);
+	
 	glColor3f(0.0, 1.0, 0.0);   //choosing green color
-	glVertex3f(1.20f, 0.20f, 0.0f);   
-	glVertex3f(0.80f, 0.20f, 0.0f);
-	glVertex3f(0.80f, 0.00f, 0.0f);
-	glVertex3f(1.20f, 0.00f, 0.0f);
+	glVertex3f(100.0f, 100.0f, 0.0f);   
+	glVertex3f(120.0f, 100.0f, 0.0f);
+	glVertex3f(120.0f, 120.0f, 0.0f);
+	glVertex3f(100.0f, 120.0f, 0.0f);
 
 	glColor3f(0.0, 0.0, 1.0);   //choosing blue color
-	glVertex3f(0.0f, -0.20f, 0.0f);   
-	glVertex3f(0.40f, -0.20f, 0.0f);
-	glVertex3f(0.40f, -0.00f, 0.0f);
-	glVertex3f(0.0f, -0.00f, 0.0f);
+	glVertex3f(25.0f, 10.0f, 0.0f);   
+	glVertex3f(100.0f, 12.0f, 0.0f);
+	glVertex3f(100.0f, 48.7f, 0.0f);
+	glVertex3f(25.0f, 48.7f, 0.0f);
 
 	glEnd();
 	glutSwapBuffers();
@@ -122,24 +163,6 @@ void idle()
 
 }
 
-void loadTexture()
-{
-	GLuint texture[1]; // declaring space for one texture
-	int twidth, theight; // declaring variable for width and height of an image
-	unsigned char* tdata; // declaring pixel data
-						  // loading image data from specific file:
-	tdata = loadPPM("auto3.ppm", &twidth, &theight);
-	if (tdata == NULL) return; // check if image data is loaded
-							   // generating a texture to show the image
-	glGenTextures(1, &texture[0]);
-	glBindTexture(GL_TEXTURE_2D, texture[0]);
-	glTexImage2D(GL_TEXTURE_2D, 0, 3, twidth, theight, 0, GL_RGB, GL_UNSIGNED_BYTE,
-		tdata);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-}
-
-
 
 int main(int argc, char** argv) {
 	/* 1) INITIALIZATION */
@@ -147,7 +170,7 @@ int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	// set window position and size
 	glutInitWindowPosition(545, 180);
-	glutInitWindowSize(720, 720);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	// set the combination of predefined values for display mode
 	// set color space (Red, Green, Blue - RGB)
 	// alocate depth buffer
